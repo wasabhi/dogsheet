a27e58266721feadb6cd17d0215921bb628e9950
Malcolm Locke <malc@wholemeal.co.nz>
2010-01-14 21:14:25 +1300

    Upstream update to jquery.select-autocomplete.js

--- public/javascripts/jquery.select-autocomplete.js
+++ public/javascripts/jquery.select-autocomplete.js
@@ -60,9 +60,25 @@
 			$input.result(function(event, selected_item, formatted) { 
 				$($this.find('option[value=' + selected_item.value + ']')[0]).attr('selected', true);
 			});
+
+      $input.blur(function(){
+          // autocomplete has removed blank options
+          // ensure that if value is removed we select the blank option if available
+          if(this.value == ""){
+            $($this.find('option[value=]')[0]).attr('selected', true);
+          }
+
+          /*   failsafe to ensure text box always represents the value being used in the select
+           *   there are edge cases where if you leave the field part way through the word, or clear the value when no blank option is available
+           *   that force a mismatch between the 2 elements
+           */
+          if(this.value != $this[0].options[$this[0].selectedIndex].text){
+            $input.val($this[0].options[$this[0].selectedIndex].text);
+          }  
+      });
 		
 			//set the initial text value of the autocomplete input box to the text node of the selected item in the select control
-			$input.val($this.find('[selected=selected]').text());
+			$input.val($this[0].options[$this[0].selectedIndex].text);
 		
 			//normally, you'd hide the select list but we won't for this demo
 			$this.hide();
