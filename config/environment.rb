098cd67944f2bdbc20c02128efd3bf0f0071c09f
Malcolm Locke <malc@wholemeal.co.nz>
2010-06-16 14:30:28 +1200

    Changed crafterm-comma dependency to comma

--- config/environment.rb
+++ config/environment.rb
@@ -25,8 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'crafterm-comma', :lib => 'comma',
-                               :source => 'http://gems.github.com/'
+  config.gem 'comma'
   config.gem 'fastercsv'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
