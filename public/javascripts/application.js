6838667a671c731189e55ca1e5175dd04703f4f5
Malcolm Locke <malc@wholemeal.co.nz>
2011-04-05 21:41:53 +1200

    Update invoice total when (de)selecting timeslices

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,9 +4,4 @@ jQuery.ajaxSetup({
 
 $(document).ready(function () {
   $('.sparkline').sparkline('html', {type: 'bar', barColor: '#383838'});
-
-  $('#timeslice_select_all').click(function () {
-    var checked = $(this).attr('checked');
-    $('td.checkbox input').attr('checked', checked);
-  });
 });
--- public/javascripts/invoiceform.js
+++ public/javascripts/invoiceform.js
@@ -2,4 +2,22 @@ $(document).ready(function () {
   $('.datepicker').datepicker({
     dateFormat:'yy-mm-dd'
   });
+
+  $('#timeslice_select_all').click(function () {
+    var checked = $(this).attr('checked');
+    $('td.checkbox input').attr('checked', checked);
+    calculateTotals();
+  });
+
+  $('input.timeslice_toggle').change(function() {calculateTotals()});
 });
+
+function calculateTotals() {
+  var total = 0.00;
+  $('tbody > tr.timeslice').each(function(index) {
+    if ($(this).find('input.timeslice_toggle:first').is(':checked')) {
+      total = total + parseFloat($(this).attr('data-cost'));
+    }
+  });
+  $('td#total').text('$' + $.currency(total));
+}
